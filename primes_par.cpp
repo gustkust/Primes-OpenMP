40f63e2b3e26f4f0993864107db083d3e82cdb85
Gustaw Kuston <gustaw.kuston@student.put.poznan.pl>
2022-03-27 13:57:43 +0200

    seq and par

--- primes_par.cpp
+++ primes_par.cpp
@@ -9,19 +9,25 @@ const int range = 1000 * 1000 * 1000;
 
 int eratosthenes(int range)
 {
+    omp_set_num_threads(4);
+
     range++;
 
     int *primes = new int[range];
 
+    #pragma omp parallel for
     for (int i = 2; i < range; i++)
         primes[i] = 1;
     
-    for (int i = 2; i * i < range; i += 1)
+    int rangeSQRT = (int) sqrt((double) range); 
+    #pragma omp parallel for schedule(dynamic)
+    for (int i = 2; i < rangeSQRT; i += 1)
         if (primes[i] == 1)
             for (int j = i * i; j < range; j += i)
                 primes[j] = 0;
 
     int res = 0;
+    #pragma omp parallel for reduction(+:res)
     for (int i = 2; i < range; i++)
         res += primes[i];
     
